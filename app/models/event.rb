26c8cc0cdd40a8a9f54c7310ce7d2dc8fd0dc3e7
Sou1Jkeee <1nfern0@list.ru>
2020-04-08 17:00:58 +0300

    pincode added

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,11 +8,17 @@ class Event < ApplicationRecord
 
   validates :user, presence: true
 
-  validates :title, presence: true, length: {maximum: 255}
+  validates :title, presence: true, length: { maximum: 255 }
   validates :address, presence: true
   validates :datetime, presence: true
 
+  validates :pincode, length: { maximum: 4 }
+
   def visitors
     (subscribers + [user]).uniq
+  end
+
+  def pincode_valid?(pin2check)
+    pincode == pin2check
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   resources :events do
     resources :comments,      only: %i[create destroy]
     resources :subscriptions, only: %i[create destroy]
-    resources :photos,         only: %i[create destroy]
+    resources :photos,        only: %i[create destroy]
+
+    post :show, on: :member
   end
 
   resources :users, only: %i[show edit update]
